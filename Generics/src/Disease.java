36da5ae5b59939c136108794e4b35ad2eb498647
s-gheldd <s-gheldd@haw-landshut.de>
2016-01-08 10:15:54 +0100

    interfaces

---
+++ Generics/src/Disease.java
@@ -0,0 +1,8 @@
+//package generics.doctors.disease;
+
+/**
+ * @author Gudrun Schiedermeier
+ * @version 2016-01-04
+ */
+public interface Disease {
+}
---
+++ Generics/src/Lovesickness.java
@@ -0,0 +1,8 @@
+//package generics.doctors.disease;
+
+/**
+ * @author Gudrun Sschiedermeier
+ * @version 2016-01-04
+ */
+public class Lovesickness implements Mental {
+}
